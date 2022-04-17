cbc256edecee46df50df0e5be76b64c3bb7d99db
Vladimir Sergeev <v.sergeev90@yandex.ru>
2022-04-17 18:50:24 +0300

    Final4

--- backend/foodgram_project/recipes/serializers.py
+++ backend/foodgram_project/recipes/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import F
 from django.contrib.auth import get_user_model
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
@@ -55,28 +56,53 @@ class RecipeSerializer(serializers.ModelSerializer):
         return data
 
     def create(self, validated_data):
-        image = validated_data.pop('image')
-        ingredients_data = validated_data.pop('ingredients')
-        recipe = Recipe.objects.create(image=image, **validated_data)
-        tags_data = self.initial_data.get('tags')
-        recipe.tags.set(tags_data)
-        self.create_ingredients(ingredients_data, recipe)
+        validated_data.pop('ingredients_amount')
+        tags = validated_data.pop('tags')
+        recipe = super().create(validated_data)
+        ingredients_amount = self.get_ingredients_list(
+            self.initial_data['ingredients'], recipe)
+        recipe.tags.set(tags)
+        recipe.ingredients_amount.set(ingredients_amount)
+        recipe.save()
         return recipe
 
     def update(self, instance, validated_data):
-        instance.image = validated_data.get('image', instance.image)
         instance.name = validated_data.get('name', instance.name)
         instance.text = validated_data.get('text', instance.text)
-        instance.cooking_time = validated_data.get(
-            'cooking_time', instance.cooking_time
-        )
-        instance.tags.clear()
-        tags_data = self.initial_data.get('tags')
-        instance.tags.set(tags_data)
-        IngredientForRecipe.objects.filter(recipe=instance).all().delete()
-        self.create_ingredients(validated_data.get('ingredients'), instance)
+        instance.cooking_time = validated_data.get('cooking_time',
+                                                   instance.cooking_time)
+        ingredients_data = validated_data.pop('ingredients_amount')
+        tags_data = validated_data.pop('tags')
+        if tags_data:
+            instance.tags.set(tags_data)
+        if ingredients_data:
+            ingredient = self.initial_data['ingredients']
+            ingredients = self.get_ingredients_list(
+                ingredient,
+                instance)
+            instance.ingredients_amount.set(ingredients)
         instance.save()
-        return
+        return instance
+
+    def get_ingredients_list(self, ingredients, recipe_id):
+        ingredients_list = []
+        ingredients_to_delete = IngredientForRecipe.objects.filter(
+            recipe=recipe_id)
+        if ingredients_to_delete:
+            for ingredient in ingredients_to_delete:
+                ingredient.delete()
+        for ingredient in ingredients:
+            ingredient_id = ingredient['id']
+            amount = ingredient['amount']
+            ingredient_instance = Ingredient.objects.get(id=ingredient_id)
+            if (IngredientForRecipe.objects.
+               filter(recipe=recipe_id, ingredient_id=ingredient_id).exists()):
+                amount += F('amount')
+            ingredient, updated = IngredientForRecipe.objects.update_or_create(
+                recipe=recipe_id, ingredient=ingredient_instance,
+                defaults={'amount': amount})
+            ingredients_list.append(ingredient)
+        return ingredients_list
 
     class Meta:
         model = Recipe
